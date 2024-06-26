112e0d0bce99fa3040940c4022ff77e3b0daefa6
CleytonSM <cleytonsm1309@gmail.com>
2024-06-26 14:35:47 -0300

    :tada: First Commit

---
+++ src/main/java/com/blueprintmate/BluePrintMateApplication.java
@@ -0,0 +1,13 @@
+package com.blueprintmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluePrintMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluePrintMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blueprintmate/BluePrintMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueprintmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluePrintMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
