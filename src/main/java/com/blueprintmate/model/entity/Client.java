d6f8a66edffd8537de05b32a5e60548eda585724
yagodaoud <yagodaoud@hotamil.com>
2024-06-28 15:04:12 -0300

    :sparkles: feat: Add client getters and setters

--- src/main/java/com/blueprintmate/model/entity/Client.java
+++ src/main/java/com/blueprintmate/model/entity/Client.java
@@ -47,4 +47,96 @@ public class Client {
 
     @Column(name = "deleted_at")
     private Timestamp deletedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+
+    public String getReligion() {
+        return religion;
+    }
+
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+
+    public boolean isVeganOrVegetarian() {
+        return isVeganOrVegetarian;
+    }
+
+    public void setVeganOrVegetarian(boolean veganOrVegetarian) {
+        isVeganOrVegetarian = veganOrVegetarian;
+    }
+
+    public String getIndividualNeeds() {
+        return individualNeeds;
+    }
+
+    public void setIndividualNeeds(String individualNeeds) {
+        this.individualNeeds = individualNeeds;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Timestamp getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Timestamp deletedAt) {
+        this.deletedAt = deletedAt;
+    }
 }
