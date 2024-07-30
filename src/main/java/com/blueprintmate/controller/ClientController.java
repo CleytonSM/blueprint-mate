209ed6304307e45d7d3e079c49af7743b57104ee
CleytonSM <cleytonsm1309@gmail.com>
2024-07-30 07:56:10 -0300

    :sparkles:feat: add client deletion route

--- src/main/java/com/blueprintmate/controller/ClientController.java
+++ src/main/java/com/blueprintmate/controller/ClientController.java
@@ -3,6 +3,7 @@ package com.blueprintmate.controller;
 import com.blueprintmate.model.dto.ClientCreateDTO;
 import com.blueprintmate.model.dto.ClientResponseDTO;
 import com.blueprintmate.model.dto.PageFilterDTO;
+import com.blueprintmate.model.dto.PageResponseDTO;
 import com.blueprintmate.service.ClientService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +31,14 @@ public class ClientController {
     }
 
     @PostMapping("/find/all")
-    public ResponseEntity<?> findAllByPagination(@RequestBody PageFilterDTO pageFilterDTO) {
+    public ResponseEntity<PageResponseDTO<ClientResponseDTO>> findAllByPagination(@RequestBody PageFilterDTO pageFilterDTO) {
         return new ResponseEntity<>(service.findAllByPagination(pageFilterDTO), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable int id) {
+        service.deleteById(id);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
