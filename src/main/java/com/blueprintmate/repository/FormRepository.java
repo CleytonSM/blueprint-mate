579b37d31c6a313539b5cf4f52f54e215afc59e7
CleytonSM <cleytonsm1309@gmail.com>
2024-07-30 15:37:14 -0300

    :sparkles: feat: add admin and user deletion route

--- src/main/java/com/blueprintmate/repository/FormRepository.java
+++ src/main/java/com/blueprintmate/repository/FormRepository.java
@@ -1,7 +1,9 @@
 package com.blueprintmate.repository;
 
 import com.blueprintmate.model.entity.Form;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -9,4 +11,8 @@ import java.util.List;
 public interface FormRepository extends JpaRepository<Form, Integer> {
     @Query("SELECT f FROM Form f WHERE f.client.name = ?1")
     List<Form> findByClientName(String clientName);
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Form f WHERE f.client.id = ?1 AND f.id = ?2")
+    void deleteByClientAndFormId(int clientId, int formId);
 }
